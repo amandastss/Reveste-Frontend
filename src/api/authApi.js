21396a5029c6745e0fbb2236c080efd01273be19
amandastss <amandassantos3103@gmail.com>
2026-04-24 10:20:07 -0300

    FEAT: aletrações página login

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,18 +1,17 @@
-import axios from 'axios'
+import api from './config'
 
-const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/'
-})
+// login
+export const loginRequest = (email, password) => {
+  return api.post('/login/', {
+    email,
+    password
+  })
+}
 
-export default {
-  async login(email, password) {
-    const response = await api.post('token/', {
-      username: email, // ⚠️ aqui depende do seu backend
-      password
-    })
-
-    localStorage.setItem('token', response.data.access)
-
-    return response.data
-  }
+// registro
+export const registerRequest = (email, password) => {
+  return api.post('/register/', {
+    email,
+    password
+  })
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
-// FONT AWESOME
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faFacebookF, faGoogle, faApple } from '@fortawesome/free-brands-svg-icons'
+const app = createApp(App);
 
-// adiciona ícones na lib
-library.add(faFacebookF, faGoogle, faApple)
+app.use(createPinia());
 
-const app = createApp(App)
-
-// registra global
-app.component('font-awesome-icon', FontAwesomeIcon)
-
-app.use(router)
-app.mount('#app')
+app.mount("#app");
